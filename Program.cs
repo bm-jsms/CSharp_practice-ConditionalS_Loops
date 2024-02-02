f32c9a2481a80eab0256d449870b2ad1af0e82d3
Josue Emaxs <josueemaxsalazar04@gmail.com>
2024-02-02 16:13:45 -0500

    conditional if

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Conditional if
+
+int totalPoints = 8;
+int totalGames = 15;
+string message = "";
+
+if (totalPoints > totalGames / 2)
+{
+    message = "You are doing great!";
+}
+else if (totalPoints == totalGames / 2)
+{
+    message = "You are doing well!";
+}
+else
+{
+    message = "You need to practice more!";
+}
+
+Console.WriteLine(message);
